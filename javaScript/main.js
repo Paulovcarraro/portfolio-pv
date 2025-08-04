119e87676918344676d6968258274661fa402e9f
Paulovcarraro <paulovcarraro@gmail.com>
2025-08-04 15:16:52 -0300

    Criação do  header, juntamente com a estilização e script de funcionamento

---
+++ javaScript/main.js
@@ -0,0 +1,15 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("show");
+});
+
+// Fecha o menu se clicar fora dele
+document.addEventListener("click", (e) => {
+  if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("show");
+  }
+});
